8b8ea0ca7e2968ca84fa890d40227709d3fef51b
Alec Clowes <aclowes@gmail.com>
2017-08-26 09:53:06 -0700

    add management command tests

--- yawn/management/tests/test_webserver.py
+++ yawn/management/tests/test_webserver.py
@@ -1,13 +1,19 @@
+import os
+import subprocess
+
 from unittest import mock
 
 from django.conf import settings
 from django.core import management
+
+import yawn
 
 from yawn.management.commands import webserver
 
 
 @mock.patch.object(webserver.WSGIApplication, 'run')
 def test_webserver(mock_run):
+    mock.patch('argparse._sys.argv', ['worker'])
     management.call_command('webserver')
     assert mock_run.call_count == 1
 
@@ -16,6 +22,11 @@ def test_static_files():
     app = webserver.get_wsgi_application()
     whitenoise = webserver.DefaultFileServer(app, settings)
     whitenoise.application = mock.Mock()
+
+    # make files to serve
+    for filename in ('index.html', 'favicon.ico'):
+        path = os.path.join(os.path.dirname(yawn.__file__), 'staticfiles', filename)
+        subprocess.check_call(['touch', path])
 
     # an API request
     whitenoise({'PATH_INFO': '/api/'}, None)
