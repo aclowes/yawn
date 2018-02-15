06c9701de244de06f8ce5c71388dbf6411456982
Alec Clowes <aclowes@gmail.com>
2018-02-14 18:15:17 -0800

    fixes to support django 2.0 and rest framework 3.7

--- yawn/settings/base.py
+++ yawn/settings/base.py
@@ -42,6 +42,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
