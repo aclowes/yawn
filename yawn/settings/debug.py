ef697dfb66b6e11a670c5325eafd1715693ceaba
Alec Clowes <aclowes@gmail.com>
2018-07-28 18:23:21 -0400

    fix setState to reset page on workflow change (#46)
    * handle anonymous user toolbar login
    * bump version

--- yawn/settings/debug.py
+++ yawn/settings/debug.py
@@ -7,3 +7,8 @@ DEBUG = True
 CSRF_COOKIE_SECURE = False
 SECURE_PROXY_SSL_HEADER = None
 SECURE_SSL_REDIRECT = False
+
+# anonymous read-only allowed
+REST_FRAMEWORK['DEFAULT_PERMISSION_CLASSES'] = [  # noqa
+    'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+]
