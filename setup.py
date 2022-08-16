780c4a759a2bd607a7c313ac2216916244c35fe6
Alec Clowes <aclowes@gmail.com>
2022-08-16 16:35:54 -0700

    switch to re_path

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         'psycopg2-binary>=2.7',
-        'django>=1.9',
+        'django>=2.2',
         'djangorestframework>=3.4',
         'whitenoise>=3.0',
         'gunicorn>=19',
